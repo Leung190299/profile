e67b6fb2cc66ec21b53f76e7c0db229b1afae901
Leung190299 <leung01628381807@gmail.com>
2022-10-22 08:42:20 +0700

    first commit

---
+++ src/common/Icon.js
@@ -0,0 +1,11 @@
+import facebook from '../assets/images/facebook.svg'
+import tiktok from '../assets/images/tiktok.svg';
+import linkedin from '../assets/images/linkedin.svg';
+import react from '../assets/images/react.svg';
+
+export default {
+    facebook,
+    tiktok,
+    linkedin,
+    react
+}
