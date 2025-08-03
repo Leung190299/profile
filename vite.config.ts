8ecc6b896fbb7afd498ca80834229e42e124dfc5
leung190299 <leung01628381807@gmail.com>
2025-08-03 23:06:15 +0700

    fix: remove NODE_ENV setting in deploy workflow and update base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/profile/' : '/',
   resolve: {
     alias: {
       '@': '/src',
